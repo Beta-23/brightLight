a7c7a7f619b06a64d0e214a6d68be70bdbeac78e
Beta-23 <alfred.dominguez@gmail.com>
2020-10-03 23:17:33 -0700

    Build eventListener for light mode toggle

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+
+// Switch Theme Event Dynamically
+function switchTheme (event) {}
+// Event Listener
+toggleSwitch.addEventListener('change', switchTheme);
