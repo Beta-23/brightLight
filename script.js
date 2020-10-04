36aa6e7be96cfb4613c99a644967b7c85ada2465
Beta-23 <alfred.dominguez@gmail.com>
2020-10-03 23:18:31 -0700

    Test boolean for checked toggle

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
 
 // Switch Theme Event Dynamically
-function switchTheme (event) {}
+function switchTheme (event) {
+	console.log(event.target.checked);
+}
 // Event Listener
 toggleSwitch.addEventListener('change', switchTheme);
